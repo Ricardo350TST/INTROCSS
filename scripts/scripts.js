90d5b4b5561a88bc98b22ade4b2409f6d97bdb1c
ricardo garcia <richard2500ruiz@gmail.com>
2024-12-10 17:10:56 -0600

    Se agregan los archivos y avances en clases de CSS

---
+++ scripts/scripts.js
@@ -0,0 +1,6 @@
+const closeButton = document.getElementById('closeBannerButton')
+const cookieBanner = document.getElementById('cookieBanner')
+
+closeButton.addEventListener('click', () => {
+    cookieBanner.classList.add()
+})
